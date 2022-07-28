42f06913ba92a719e3ff14384389aea22229d509
Aliaksandr Khramau <axelkhr@gmail.com>
2022-07-28 14:44:27 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let i = str.length >> 1;
+  while ((i > 0) && (str.length > 0)) {
+    bracketsConfig.forEach(element => {
+      str = str.replace(element[0] + element[1], '');
+    });
+    i--;
+  }
+  return !(str.length > 0);
 }
